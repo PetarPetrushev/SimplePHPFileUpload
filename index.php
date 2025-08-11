2035e34b4b94aeead0d8bb327cf96ad28a312328
Peter <petar.p@myemailhosts.com>
2025-08-11 22:40:14 +0100

    Update index.php
    Removed meta viewport for better mobile experience

--- index.php
+++ index.php
@@ -190,7 +190,6 @@ if(isset($_GET['download'])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
         if(isset($_GET['viewFile'])){
                     $jsonPath = Uploaded_Files_Dir . '/' . basename($_GET['viewFile']) . '.json';
